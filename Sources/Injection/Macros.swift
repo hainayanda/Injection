0dad754c2761462293cfa0cb57d6e23166f7ca73
Nayanda Haberty <hainayanda@outlook.com>
2025-02-23 10:49:35 +0700

    Make the macro only available for macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0 and newer

--- Sources/Injection/Macros.swift
+++ Sources/Injection/Macros.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+@available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
 @attached(extension, conformances: Injectable, names: arbitrary)
 public macro Injectable() = #externalMacro(
     module: "InjectionMacro", type: "InjectableMacro"
